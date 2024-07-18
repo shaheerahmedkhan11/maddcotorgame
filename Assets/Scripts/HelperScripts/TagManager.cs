76a31bfe92612f0b0a8da6508715f040174cde90
shaheerahmedkhan11 <shaheerahmedkhan11@gmail.com>
2024-07-18 12:58:11 +0500

    player is shooting

--- Assets/Scripts/HelperScripts/TagManager.cs
+++ Assets/Scripts/HelperScripts/TagManager.cs
@@ -9,8 +9,8 @@ public static class TagManager
     public static string HORIZONTAL_AXIS = "Horizontal";
     public static string VERTICAL_AXIS = "Vertical";
 
-    public static string IDLE_ANIMATION_NAME = "Idle";
-    public static string WALK_ANIMATION_NAME = "Walk";
+    public static string IDLE_ANIMATION_NAME = "Idle player";
+    public static string WALK_ANIMATION_NAME = "Player walk";
     public static string DEATH_ANIMATION_NAME = "Death";
     public static string SHOOT_ANIMATION_NAME = "Shoot";
     public static string ATTACK_ANIMATION_NAME = "Attack";
--- Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -10,17 +10,34 @@ public class PlayerMovement : MonoBehaviour
     private float minbound_X = -71f, maxbound_X = 71f, minbound_Y = -3.3f, maxbound_Y = 0f;
     private Vector3 tempPos;
     private float xAxis, yAxis;
-
+    private PlayerAnimation playerAnimation;
+    [SerializeField]
+    private float shootWaitTime = 0.5f;
+    private float waitBeforeShooting;
+    [SerializeField]
+    private float moveWaitTime = 0.3f;
+    private float waitBeforeMoving;
+    private bool canMove = true;
+    private void Awake()
+    {
+        playerAnimation = GetComponent<PlayerAnimation>();
+    }
     // Update is called once per frame
     void Update()
-    {
+    { 
         HandleMovement();
+        HandleAnimation();
+        HandleFacingDirection();
+        HandleShooting();
+        CheckIfCanMove();
     }
     void HandleMovement()
     {
         xAxis = Input.GetAxisRaw(TagManager.HORIZONTAL_AXIS);
         yAxis = Input.GetAxisRaw(TagManager.VERTICAL_AXIS);
 
+        if (!canMove)
+            return;
         tempPos = transform.position;
 
         tempPos.x += xAxis * moveSpeed * Time.deltaTime;
@@ -35,4 +52,53 @@ public class PlayerMovement : MonoBehaviour
             tempPos.y = maxbound_Y;
         transform.position = tempPos;
     }
+    void HandleAnimation()
+    {
+        if (!canMove)
+            return;
+        Debug.Log("xAxis: " + xAxis + " yAxis: " + yAxis);
+        if (Mathf.Abs(xAxis) > 0 || Mathf.Abs(yAxis) > 0)
+        {
+            Debug.Log("Playing walk animation: " + TagManager.WALK_ANIMATION_NAME);
+            playerAnimation.PlayAnimation(TagManager.WALK_ANIMATION_NAME);
+        }
+        else
+        {
+            Debug.Log("Playing idle animation: " + TagManager.IDLE_ANIMATION_NAME);
+            playerAnimation.PlayAnimation(TagManager.IDLE_ANIMATION_NAME);
+        } 
+    }
+    void HandleFacingDirection()
+    {
+        if (xAxis > 0)
+            playerAnimation.SetFacingDirection(true);
+        else if (xAxis < 0)
+            playerAnimation.SetFacingDirection(false);
+    }
+    void StopMovement()
+    {
+        canMove = false;
+        waitBeforeMoving = Time.time + moveWaitTime;
+    }
+    void Shoot()
+    {
+        waitBeforeShooting = Time.time + shootWaitTime;
+        StopMovement();
+        playerAnimation.PlayAnimation(TagManager.SHOOT_ANIMATION_NAME);
+    }
+    void CheckIfCanMove()
+    {
+        if (Time.time > waitBeforeMoving)
+            canMove = true;
+    }
+    void HandleShooting()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            if (Time.time > waitBeforeShooting)
+                Shoot();
+        }
+
+    }
+
 }
