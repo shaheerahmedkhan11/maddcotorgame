0cb4670952bc6671968fe0badde244815885399a
shaheerahmedkhan11 <shaheerahmedkhan11@gmail.com>
2024-07-21 10:52:48 +0500

    Enemy is now walking

--- Assets/Scripts/Enemy Scripts/Enemy.cs
+++ Assets/Scripts/Enemy Scripts/Enemy.cs
@@ -16,21 +16,40 @@ public class Enemy : MonoBehaviour
 
     private void Awake()
     {
+        Debug.Log("The Awake function is being called!");
         playerTarget = GameObject.FindWithTag(TagManager.PLAYER_TAG).transform;
         enemyAnimation = GetComponent<PlayerAnimation>();
+        if (playerTarget == null)
+        {
+            Debug.LogError("Player target not found! Make sure the player has the correct tag.");
+        }
+        if (enemyAnimation == null)
+        {
+            Debug.LogError("PlayerAnimation component not found! Make sure the enemy has a PlayerAnimation component.");
+        }
     }
+    private void Start()
+    {
+        Debug.Log("Enemy script Start called");
+    }
+
     private void Update()
     {
+        Debug.Log("Enemy script Update called");
         SearchForPlayer();
     }
     void SearchForPlayer()
     {
         if (!playerTarget)
             return;
-        if (Vector3.Distance(transform.position, playerTarget.position) > stoppingDistance)
+        float distanceToPlayer = Vector3.Distance(transform.position, playerTarget.position);
+        if (distanceToPlayer > stoppingDistance)
         {
+            Debug.Log("Distance to player: " + distanceToPlayer);
+
             transform.position = Vector2.MoveTowards(transform.position, playerTarget.position, moveSpeed * Time.deltaTime);
-            enemyAnimation.PlayAnimation(TagManager.ENEMY_Walk_ANIMATION);
+            Debug.Log("Moving towards player, playing walk animation: " + TagManager.WALK_ANIMATION_NAME);
+            enemyAnimation.PlayAnimation(TagManager.WALK_ANIMATION_NAME);
         }
         
 
